d9c435390a902bb4d5c2ddf586273988e9d7993a
Braden <bradenlbender@gmail.com>
2020-09-18 13:06:25 -0700

    Add existing files to repository

---
+++ maingame.py
@@ -0,0 +1,11 @@
+sys.path.append(".")
+
+from story_elements import storyElements
+
+print ('A party of adventurers sits down at a campfire.\n')
+print ('They\'re very tired from having adventures all day.\n')
+print ('The fire is crackling, and all seems to be okay\n')
+print ('until the bard pulls out a lute. They strum a chord.\n')
+print ('The bard begins to weave a tale of dragons, damsels, kings\n')
+print ('and queens.\n')
+
---
+++ story_elements.py
@@ -0,0 +1,16 @@
+import random
+
+class storyElements:
+    def __init_(self, badGuy, goodGuy, damsel, treasure, location, fortress)
+        self.badGuy = badGuy
+        self.goodGuy = goodGuy
+        self.damsel = damsel
+        self.treasure = treasure
+        self.location = location
+        self.fortress = fortress
+
+    def pickElements(self):
+        badGuys = ['Dragon', 'Space Dragon', 'Radioactive Octopus', 'Centaur Stack', 'Bundle of Plastic Straws', 'Box of Cheddar Cheese Crackers', 'Karen']
+        goodGuys = ['Princess', 'Space Prince', 'Space Princess', 'Prince' , 'Bounty Hunter', 'Wizard']
+        damsels = ['Princess', 'Litter of Kittens', 'Sea Turtle', 'Expensive Smartphone', 'Bag of Dice']
+        treasures = [
