e5bd5416ce44ffd0c77822e986004f91eac6ef57
Braden <bradenlbender@gmail.com>
2020-09-18 14:02:39 -0700

    Completed lists of story elements

--- story_elements.py
+++ story_elements.py
@@ -13,4 +13,13 @@ class storyElements:
         badGuys = ['Dragon', 'Space Dragon', 'Radioactive Octopus', 'Centaur Stack', 'Bundle of Plastic Straws', 'Box of Cheddar Cheese Crackers', 'Karen']
         goodGuys = ['Princess', 'Space Prince', 'Space Princess', 'Prince' , 'Bounty Hunter', 'Wizard']
         damsels = ['Princess', 'Litter of Kittens', 'Sea Turtle', 'Expensive Smartphone', 'Bag of Dice']
-        treasures = [
+        treasures = ['Eye of Mahnicuh', 'Call of Lemmy', 'Rod of Beatings', 'Remote of Television', 'Ball of Radiance']
+        locations = ['Reasonably Priced Condo', 'Lavish Mansion', 'Generic Castle', 'A Pillow Fortress', 'Overpriced Apartment']
+        fortresses = ['Spooky Spaceship', 'Creepy Call Center', 'Ominous Office Supply Store', 'Hair-raising Hospital', 'Dreadful DMV Office'] 
+
+        badGuy = random.choice(badGuys)
+        goodGuy = random.choice(goodGuys)
+        damsel = random.choice(damsels)
+        treasure = random.choice(treasures)
+        location = random.choice(locations)
+        fortress = random.choice(fortresses)
